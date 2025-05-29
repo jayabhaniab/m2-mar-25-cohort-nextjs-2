c3c01529bd0d61faf21c5b87992a9de057c43499
jayabhaniab <jay.abhani@almabetter.com>
2025-05-29 10:04:43 +0530

    29-05-2025

---
+++ optimization/optimization-app/src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <h1>Welcome to next.js optimization!</h1> */}
+      {/* <img src="/sample.jpg" alt="sample image" height={200} width={400} />
+      <img src="/sample2.jpg" alt="sample2 image" height={200} width={400} /> */}
+      {/* <Image src="/sample.jpg" alt="sample image" height={200} width={400} /> */}
+      {/* <Image src="/sample.jpg" alt="sample image" height={200} width={400} />
+      <Image src="/sample2.jpg" alt="sample2 image" height={200} width={400} priority /> */}
+      {/* <Image src="/sample2.jpg" alt="sample2 image" height={800} width={1200} /> */}
+      {/* <Image src="https://images.unsplash.com/photo-1740918417127-100b62f7889d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="sample image" height={200} width={400} /> */}
+    </div>
+  );
+}
